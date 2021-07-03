bca19c49229ee7c448786b73763d6a8b0fd80f8f
Ivan Reinaldo <reinaldoivan777@gmail.com>
2021-07-04 04:10:10 +0700

    dynamic table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import axios from "axios";
 import ExportDropdown from "./ExportDropdown";
+import { DataMaster } from "./DataMaster";
+import {
+  FormControlLabel,
+  FormGroup,
+  FormControl,
+  Checkbox,
+  FormLabel,
+} from "@material-ui/core";
 
 export default function App() {
   const [people, setPeople] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [stateMaster, setStateMaster] = useState(DataMaster);
 
   const getPeople = async () => {
     setIsLoading(true);
@@ -23,16 +32,59 @@ export default function App() {
     setPeople(fullData);
     setIsLoading(false);
     const dataCSV = {
-      column: ["name", "gender"],
-      data: fullData.map((item) => [item.name, item.gender]),
+      column: stateMaster
+        .filter((master) => master.active)
+        .map((master) => master.header),
+      data: fullData.map((item) => {
+        return stateMaster
+          .filter((master) => master.active)
+          .map((master) => item[master.value]);
+      }),
     };
 
-    const dataExcel = fullData.map((item) => ({
-      Name: item.name,
-      Gender: item.gender,
-    }));
+    const dataExcel = fullData.map((item) => {
+      const objectData = {};
+      stateMaster
+        .filter((master) => master.active)
+        .map((master) => {
+          objectData[master.header] = item[master.value];
+          return master;
+        });
+      return objectData;
+    });
+
+    console.log(dataExcel);
 
     return { dataExcel, dataCSV };
+  };
+
+  const getPeopleNoExport = async () => {
+    setIsLoading(true);
+    let nextPage = true;
+    let page = 1;
+    const fullData = [];
+    while (nextPage) {
+      const response = await axios.get(
+        `https://swapi.dev/api/people?page=${page}`
+      );
+      fullData.push(...response.data.results);
+      if (response.data.next === null) nextPage = false;
+      else page++;
+    }
+    setPeople(fullData);
+    setIsLoading(false);
+  };
+
+  const handleMasterChange = (event) => {
+    let copyMaster = [...stateMaster];
+
+    copyMaster = copyMaster.map((master) => {
+      if (event.target.name === master.value)
+        return Object.assign({}, master, { active: event.target.checked });
+      return master;
+    });
+
+    setStateMaster(copyMaster);
   };
 
   return (
@@ -47,12 +99,51 @@ export default function App() {
           set: () => setShowDropdown(!showDropdown),
         }}
       />
-      <button onClick={getPeople}>Get People</button>
-      <ul>
-        {people?.map((item, index) => (
-          <li key={index}>{item.name}</li>
-        ))}
-      </ul>
+      <button onClick={getPeopleNoExport}>Get People</button>
+      <div>
+        <FormControl component="fieldset">
+          <FormLabel>Set Master Data</FormLabel>
+          <FormGroup>
+            {stateMaster.map((master) => (
+              <FormControlLabel
+                key={master.value}
+                label={master.header}
+                control={
+                  <Checkbox
+                    checked={master.active}
+                    name={master.value}
+                    onChange={handleMasterChange}
+                  />
+                }
+              />
+            ))}
+          </FormGroup>
+        </FormControl>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            {stateMaster.map(
+              (master, index) =>
+                master.active && <td key={index}>{master.header}</td>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {people.map((item, index) => (
+            <tr key={index}>
+              {stateMaster.map(
+                (master, index) =>
+                  master.active && (
+                    <td key={`${item[master.value]}-${index}`}>
+                      {item[master.value]}
+                    </td>
+                  )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/DataMaster.js
@@ -0,0 +1,37 @@
+export const DataMaster = [
+  {
+    header: "Name",
+    value: "name",
+    active: true,
+  },
+  {
+    header: "Height",
+    value: "height",
+    active: true,
+  },
+  {
+    header: "Mass",
+    value: "mass",
+    active: true,
+  },
+  {
+    header: "Hair Color",
+    value: "hair_color",
+    active: true,
+  },
+  {
+    header: "Skin Color",
+    value: "skin_color",
+    active: true,
+  },
+  {
+    header: "Gender",
+    value: "gender",
+    active: true,
+  },
+  {
+    header: "Birth Year",
+    value: "birth_year",
+    active: true,
+  },
+];
